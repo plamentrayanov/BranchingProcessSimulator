c16a1e92d68367cd32a3d7474079b1d439b1433c
Plamen I. Trayanov <42202908+plamentrayanov@users.noreply.github.com>
2020-04-21 12:35:20 +0300

    Add files via upload

--- COVID19 Models/readCSVData.m
+++ COVID19 Models/readCSVData.m
@@ -1,6 +1,7 @@
 function [dates, newcases, totalcases] = readCSVData(CSVFile, CountryNames)
 %% reads the data in a table
-T = readtable(CSVFile,'Format','%{dd/MM/yyyy}D%f%f%f%f%f%q%q%q%f', 'Delimiter', ',', 'HeaderLines', 0, 'ReadVariableNames', true);
+% if the format of data.csv changes, you need to edit this line:
+T = readtable(CSVFile,'Format','%{dd/MM/yyyy}D%f%f%f%f%f%q%q%q%f%q', 'Delimiter', ',', 'HeaderLines', 0, 'ReadVariableNames', true);
 if iscellstr(CountryNames) && length(CountryNames)>1
     for i=1:length(CountryNames)
         T_slice = T((strcmp(T.countriesAndTerritories, CountryNames{i})),:);

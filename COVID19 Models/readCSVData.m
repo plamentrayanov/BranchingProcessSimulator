219150046214e860078600c9b2806d9f75268099
Plamen I. Trayanov <42202908+plamentrayanov@users.noreply.github.com>
2020-04-30 15:30:55 +0300

    Add files via upload

--- COVID19 Models/readCSVData.m
+++ COVID19 Models/readCSVData.m
@@ -1,23 +1,21 @@
-function [dates, newcases, totalcases] = readCSVData(CSVFile, CountryNames)
+function [dates_extended, newcases_extended, totalcases_extended] = readCSVData(CSVFile, CountryNames)
 %% reads the data in a table
 % if the format of data.csv changes, you need to edit this line:
 T = readtable(CSVFile,'Format','%{dd/MM/yyyy}D%f%f%f%f%f%q%q%q%f%q', 'Delimiter', ',', 'HeaderLines', 0, 'ReadVariableNames', true);
-if iscellstr(CountryNames) && length(CountryNames)>1
-    for i=1:length(CountryNames)
-        T_slice = T((strcmp(T.countriesAndTerritories, CountryNames{i})),:);
-        dates{i} = datenum(T_slice.year, T_slice.month, T_slice.day);
-        newcases{i} = T_slice.cases;
-    end
-else
-    T_slice = T((strcmp(T.countriesAndTerritories, CountryNames)),:);
-    [dates, dates_sorted_ind] = sort(datenum(T_slice.year, T_slice.month, T_slice.day));
-    newcases = T_slice.cases(dates_sorted_ind);
-    totalcases = cumsum(newcases);
-    
-    dates = dates(totalcases~=0);
-    newcases = newcases(totalcases~=0);
-    totalcases = totalcases(totalcases~=0);
-end
+
+T_slice = T((strcmp(T.countriesAndTerritories, CountryNames)),:);
+[dates, dates_sorted_ind] = sort(datenum(T_slice.year, T_slice.month, T_slice.day));
+newcases = T_slice.cases(dates_sorted_ind);
+
+dates_extended = dates(1):dates(end);
+newcases_extended = zeros(size(dates_extended));
+newcases_extended(arrayfun(@(x)(any(dates==x)), dates_extended))=newcases;
+totalcases_extended = cumsum(newcases_extended);
+
+dates_extended = dates_extended(totalcases_extended~=0)';
+newcases_extended = newcases_extended(totalcases_extended~=0)';
+totalcases_extended = totalcases_extended(totalcases_extended~=0)';
+
 end
 
 

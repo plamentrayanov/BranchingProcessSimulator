dd0e591be8737b1b946a60636a6ba7cc949150d4
plamentrayanov <plamentrayanov@gmail.com>
2020-05-06 16:04:46 +0300

    deleting directory

--- COVID19 Models/Bulgaria/Im_function.m
+++
@@ -1,5 +0,0 @@
-function Im_matrix = Im_function(Im_mass, Im_age_struct_prob)
-Im_matrix=zeros(size(Im_age_struct_prob,1), 1, size(Im_mass,2));
-Im_matrix(:,1,:)=mnrnd(binornd(80,Im_mass'), Im_age_struct_prob)';
-end
-
